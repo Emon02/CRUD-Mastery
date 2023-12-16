b9dcddf6ae5883ffd555a4c53c71f2a8d3fdd17e
emon02 <abdulalimemon11@gmail.com>
2023-12-17 02:10:55 +0600

    Set up eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 // parser
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 // application routes
 const getAController = (req: Request, res: Response) => {
-  res.send("Hello World!");
-};
+  res.send('Hello World!')
+}
 
-app.get("/", getAController);
+app.get('/', getAController)
 
-export default app;
+export default app
