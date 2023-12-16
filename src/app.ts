4dff98813e3a23382848f342b23418b748027ba6
emon02 <abdulalimemon11@gmail.com>
2023-12-17 01:46:10 +0600

    connect to DB

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// parser
+app.use(express.json());
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// application routes
+const getAController = (req: Request, res: Response) => {
+  res.send("Hello World!");
+};
+
+app.get("/", getAController);
+
+export default app;
